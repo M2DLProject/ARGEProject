5a9bb22e24b4f7d291abe8ea6a84586bec969fbb
smagras <steve.magras@gmail.com>
2016-04-22 16:33:19 +0200

    jar for vm

--- WorkerNode/src/Calculator.java
+++ WorkerNode/src/Calculator.java
@@ -1,11 +1,6 @@
 public class Calculator {
 	public int add(int i1, int i2) {
 
-		try {
-			Thread.sleep(5000); // 1000 milliseconds is one second.
-		} catch (InterruptedException ex) {
-			Thread.currentThread().interrupt();
-		}
 		return i1 + i2 + 10;
 	}
 
