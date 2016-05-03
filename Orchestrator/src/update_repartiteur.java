3beffebee0afb3bb00110e438ba98615e0ad9bce
smagras <steve.magras@gmail.com>
2016-05-03 21:28:58 +0200

    jar

--- Orchestrator/src/update_repartiteur.java
+++ Orchestrator/src/update_repartiteur.java
@@ -97,7 +97,7 @@ public class update_repartiteur {
 		}
 
 		System.out.println("Associate VM to ip");
-		System.out.println("[" + server.getName() + "]" + netFloatingIP.getFloatingIpAddress());
+		System.out.println("[" + server.getInstanceName() + "]" + netFloatingIP.getFloatingIpAddress());
 
 		// Boot the Server
 
