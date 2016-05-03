e8cfa4662dd485438b0be3a83292d751b24acf5b
vlaks <roletto.dylan@gmail.com>
2016-05-03 22:42:41 +0200

    test host

--- Orchestrator/src/update_repartiteur.java
+++ Orchestrator/src/update_repartiteur.java
@@ -135,7 +135,7 @@ public class update_repartiteur {
 		boolean isFound = false;
 		int it = 0;
 		while(!isFound && it < servers.size()){
-			System.out.println(it+" "+servers.get(it).getAccessIPv4());
+			System.out.println(it+" "+servers.get(it).getHost());
 			if(servers.get(it).getAccessIPv4().equals(ip)) {
 				String wNodeId = servers.get(it).getId();
 				os.compute().servers().delete(wNodeId);
