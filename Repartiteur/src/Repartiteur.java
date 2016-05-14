7f8c3f2f763d9e801c27f71a7bfbdf91e3514d43
smagras <steve.magras@gmail.com>
2016-05-14 19:55:16 +0200

    jar

--- Repartiteur/src/Repartiteur.java
+++ Repartiteur/src/Repartiteur.java
@@ -44,18 +44,10 @@ public class Repartiteur {
 
 	public int call(String method, int i1, int i2) throws MalformedURLException, XmlRpcException {
 
-		System.out.println("Repartiteur is call!");
-
 		// make the a regular call
 		Object[] params = new Object[] { new Integer(i1), new Integer(i2) };
 		return repartiteurHelper.callMethod(method, params);
 
-		// make a call using dynamic proxy
-		/*
-		 * ClientFactory factory = new ClientFactory(client); Adder adder =
-		 * (Adder) factory.newInstance(Adder.class); int sum = adder.add(2, 4);
-		 * System.out.println("2 + 4 = " + sum);
-		 */
 	}
 
 	public int addWN(String ip, String port) {
